6a9bc02db1ecf44ef571ba60cb7dfeaebd2d6702
dangasabonu <139097898+dangasabonu@users.noreply.github.com>
2023-07-29 09:41:24 +0500

    Add files via upload

---
+++ umumiy.py
@@ -0,0 +1,53 @@
+# Umumiy vazifalar
+# 3-dars
+
+# 1
+# 5 ning 4-darajasini toping.
+print(5**4)
+
+# 2
+# 22 ni 4 ga bolganda nechi qoldiq qoladi?
+print(22 % 4)
+
+# 3
+# Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping.
+print(f"Kvadrat yuzi={125*125},\nKvadrat perimetri=125*4")
+
+# 4
+# Diametri 12 ga teng bo'lgan doiraning yuzini toping
+print(f"S = {(12/2)**2*3.14}")
+
+# 5
+# Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini top
+print(f"Gipotenuza={(6**2+7**2)**(1/2)}")
+
+
+# 4-dars
+
+# 1
+soz = "Hello World!"
+print(soz)
+
+# 2
+xabar = "Hozir yoki hech qachon!"
+print(xabar)
+xabar = "Bu g\'irt ahmoqklik!"
+print(xabar)
+
+# 3
+# class = 4:
+
+# 4
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi", radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+
+# 5-dars
+
+# 1
+kocha = "Bog'bon"
+mahalla = "Sog'bon"
+tuman = "Bodomzor"
+viloyat = "Samarqand"
